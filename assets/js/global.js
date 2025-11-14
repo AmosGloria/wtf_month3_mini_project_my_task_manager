e973495735430a6168b814f2275f01fe0effd374
Amos Gloria <amoskesegloria.com>
2025-11-14 23:26:27 +0100

    Dynamically loaded html, css and js files in components and templates

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,28 @@
+const componentElements = document.querySelectorAll('[data-import]');
+
+for(const element of componentElements){
+    // console.log(`componentElements found:`, element);
+    const fileImport = element.getAttribute('data-import');
+    // console.log(fileImport);
+
+    fetch(fileImport)
+    .then(response=>response.text())
+    .then(component =>{
+        element.innerHTML=component;
+    })
+    .catch(err=>{
+        error(`Error loading component from ${fileImport}:`, err);  
+    })
+}
+
+function loadStyles(){
+   const link = document.createElement('link');
+   link.rel='stylesheet';
+   link.href='/assets/css/global.css';
+   document.head.appendChild(link); 
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const components = ['bubbles', 'button', 'card'];
 
