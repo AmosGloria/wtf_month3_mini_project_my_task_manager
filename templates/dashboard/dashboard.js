bc8dc30dc74cefec7cea4cd33cd93b756084366f
Amos Gloria <amoskesegloria.com>
2025-12-01 09:41:56 +0100

    Updated task file

--- templates/dashboard/dashboard.js
+++ templates/dashboard/dashboard.js
@@ -1,5 +1,7 @@
 const taskButton = document.getElementById('task');
 
+
 taskButton.addEventListener("click", () => {
     window.location.href = "/templates/task-management/task.html";
 });
+
---
+++ templates/task-management/task.js
@@ -0,0 +1,4 @@
+const dashboardButton = document.getElementById('dashboardButton')
+dashboardButton.addEventListener('click', ()=>{
+   window.location.href = "/templates/dashboard/dashboard.html";  
+})
